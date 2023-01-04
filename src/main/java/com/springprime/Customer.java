4786a89657299ef43ae1df0405bb797a51b692a2
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-01-04 21:16:14 +0200

    [Add] Model Instantiation

---
+++ src/main/java/com/springprime/Customer.java
@@ -0,0 +1,50 @@
+package com.springprime;
+
+public class Customer {
+    private Integer id;
+    private String name;
+    private String email;
+    private Integer age;
+
+    public Customer(Integer id, String name, String email, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Customer() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
