0af3f9d1731f6ae42bd0c1a7dea3fb1efb8f7ec7
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-01-04 20:00:22 +0200

    [update] Get Mapping exploitation

--- src/main/java/com/springprime/Main.java
+++ src/main/java/com/springprime/Main.java
@@ -2,10 +2,17 @@ package com.springprime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args){
         SpringApplication.run(Main.class, args);
+    }
+    @GetMapping("/")
+    public String greet () {
+        return "Hello";
     }
 }
