d9c6dd7df6071fd95f52cb2c94c450269d69294c
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-01-04 19:25:08 +0200

    [add] project initials

---
+++ src/main/java/com/cedrick/springbootstarterpacket/SpringBootStarterPacketApplication.java
@@ -0,0 +1,13 @@
+package com.cedrick.springbootstarterpacket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterPacketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterPacketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cedrick/springbootstarterpacket/SpringBootStarterPacketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cedrick.springbootstarterpacket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterPacketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
