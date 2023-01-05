ea2c38cb53cc564def7e3cdbceb51d19573959b8
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-01-06 00:00:51 +0200

    [update] Entity and table mapping initials

--- src/main/java/com/springprime/Customer.java
+++ src/main/java/com/springprime/Customer.java
@@ -1,6 +1,19 @@
 package com.springprime;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Customer {
+
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
     private Integer id;
     private String name;
     private String email;
